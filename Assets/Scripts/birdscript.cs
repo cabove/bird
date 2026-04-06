436953cad8c561fa8c6e103584a4e8c31bdbe12d
Camille Bove <cabove@andrew.cmu.edu>
2026-04-05 20:02:47 -0400

    bru

--- Assets/Scripts/birdscript.cs
+++ Assets/Scripts/birdscript.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class birdscript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 2f;
+    public float jumpForce = 7f;
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.1f;
+
+    private Rigidbody2D rb;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+    }
+
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        return hit.collider != null;
     }
 }
